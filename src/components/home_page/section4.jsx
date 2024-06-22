c4864f994b99c9877e2b06abf2990fc8276a3719
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-06-22 23:50:25 +0330

    section4 background set

--- src/components/home_page/section4.jsx
+++ src/components/home_page/section4.jsx
@@ -2,8 +2,18 @@ import styles from '@/app/page.module.css'
 
 const Section4 = () => {
     return(
-        <div>
-            Welcome to Section 4
+        <div className={styles.section4}>
+            <div className={styles.footerBg}></div>
+            <div className={styles.contactForm}>
+                <p>Any questions?</p>
+                <p>Let’s talk today!</p>
+                <form>
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         </div>
     )
 }
