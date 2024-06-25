39f166c8aabce698ee2b79b9f4ed34f9964fc7e1
Hossein Abedi <hosein.abedi2000@gmail.com>
2024-06-25 12:35:48 +0330

    Add youtube video to section1

--- src/components/home_page/section1.jsx
+++ src/components/home_page/section1.jsx
@@ -13,10 +13,11 @@ const Section1 = () => {
                 <button>About us</button>
             </div>
             <div className={styles.video}>
-                <video controls poster="/video.png">
-                    <source src="https://www.youtube.com/watch?v=LXb3EKWsInQ"  />
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/LXb3EKWsInQ?si=5qPR8FxPIg9wZ7jt" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                {/* <video controls poster="/video.png">
+                    <source src="https://www.youtube.com/embed/tgbNymZ7vqY"  />
                     Your browser does not support the video tag.
-                </video>
+                </video> */}
                 {/* <iframe style={{minHeight: "180px", width: "100%"}}
                     src="https://www.youtube.com/watch?v=LXb3EKWsInQ">
                 </iframe> */}
